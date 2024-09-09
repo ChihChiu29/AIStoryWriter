60b00aa6f54054835e97986096383a60f95c2276
Chih Chiu <chih.chiu.19@gmail.com>
2024-09-09 01:38:51 -0700

    changed temp for json back to 0.1 to avoid inf loop

--- Writer/Interface/Wrapper.py
+++ Writer/Interface/Wrapper.py
@@ -267,7 +267,9 @@ class Interface:
 
                 # if temperature is not set, set it to 0 for JSON mode
                 if "temperature" not in ModelOptions:
-                    ModelOptions["temperature"] = 0
+                    # ModelOptions["temperature"] = 0
+                    # Avoid inf loop.
+                    ModelOptions["temperature"] = 0.1
                 _Logger.Log("Using Ollama JSON Format", 4)
             else:
                 # Let's see if this would reduce the chance that it passes the millions while loop.
